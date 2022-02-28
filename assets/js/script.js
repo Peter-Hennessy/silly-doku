b53539a419dac31d015bdd2d69992b04ad5fd07e
Peter Hennessy <peternesta@hotmail.com>
2022-02-28 10:36:40 +0000

    Manually added content for Suduko puzzle

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+//Load board manually
+
+const beginner = [
+     "F------7------5-2------1---CF2----H1—9F-----71--9-D-5-2---F51---7H----CD5-------",
+"FH5C2917D971DH5C2F2CD7F1H59CF257D9H15D9F1H7C271H29CDF5H2C9DF517197H52FCD5F1C729H" 
+]; 
+
+const intermediate = [
+     "--9-------D----F-75H-C1----15--D-CF-------D-H----9-------75----C-------1--2--C--", 
+     "F19D725HC2DC9H5F175H7C1F92D15H2D7CF992F5C1D7H7CDF9H152H9175D2CFCF5H297D1D721FCH95" 
+    ]; 
+
+const expert = [ 
+    "-1-5-------97-D2----5----7-5---C---7-F--2-D1---H--5---1-D------2-C-----9-7----H--", 
+    "7125HCF9DFC971D25HHD52F917C521DCF9H7CF792HD15D9H175C2F1HDF975C225CHD17F997FC52HD1" 
+];
+
+
