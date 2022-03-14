9c096714c8983fc2a600b6a678c42d54272b1d1b
Peter Hennessy <peternesta@hotmail.com>
2022-03-14 23:53:26 +0000

    Test all squares, found bud in solution format. correct two, one remaining

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,16 @@
 //Load board manually
 
 const beginner = [
-    "F------7------5-2------1---CF2----H1--9F-----71--9-D-5-2---F51---7H----CD5-------",
-    "FH5C2917D971DH5C2F2CD7F1H59CF257D9H15D9F1H7C271H29CDF5H2C9DF517197H52FCD5F1C729H"
-];
+    "F------7------5-2------1---CF2----H1-9F-----71--9-D-5-2---F51---7H----CDD5---7---",
+    "FH5C2917D971DH5C2F2CD7F1H59CF257D9H15D9F1H7C271H29CDF5H2C9DF517197H52FCDD5F1C729H" 
+]; 
+
 const intermediate = [
-    "--9-------D----F-75H-C1----15--D-CF-------D-H----9-------75----C-------1--2--C--",
+    "--9-------D----F-75H-C1----15--D-CF-------D-H----9H-------75----C-------1--2--C--",
     "F19D725HC2DC9H5F175H7C1F92D15H2D7CF992F5C1D7H7CDF9H152H9175D2CFCF5H297D1D721FCH95"
 ];
 const expert = [
-    "-1-5-------97-D2----5----7-5---C---7-F--2-D1---H--5---1-D------2-C-----9-7----H--",
+    "-125HCF9DFC971D25HHD52F917C521DCF9H7CF792HD15D9H175C2F1HDF975C225CHD17F997FC52HD1",
     "7125HCF9DFC971D25HHD52F917C521DCF9H7CF792HD15D9H175C2F1HDF975C225CHD17F997FC52HD1"
 ];
 
@@ -168,6 +169,11 @@ function updateMove() {
             //Clear the selected Variables
             selectedNum = null;
             selectedTile = null;
+            //Check if board is completed
+            if (checkDone()) {
+                endGame();
+             }
+
             // If the number/letter does not match the solution key
         } else{
             //Disable Seletin a new number/letter for one second
@@ -184,7 +190,7 @@ function updateMove() {
                 } else {
                       //If lives is not equal to zero
                       // Update lives text
-                      id ("lives").textContent = "Lives Remaining"  +  lives;
+                      id ("lives").textContent = "Lives Remaining: "  + lives;
                       //Re-enable selecting Number & tiles
                       disableSelect = false;
                 }
@@ -202,13 +208,34 @@ function updateMove() {
         }
     }
 }
+function checkDone() {
+    let tiles = qsa(".tile");
+    for (let i = 0; i < tiles.length; i++) {
+        if (tiles[i].textContent === "") return false;
+
+    }
+    return true;
+}
+
+function endGame() {
+    //Disable moves & stops timer
+    disableSelect = true;
+    clearTimeout(timer);
+    //Display win or loose message
+    if (lives === 0 || timeRemaining === 0) {
+        id("lives").textContent = "Game Over!";
+    }else {
+        id("lives").textContent = "BOOM-SHAKA-LAKA YOU WON!!!";
+    }
+    }
+
 
 function checkCorrect(tile) {
     //Set solution based od level selected
     let solution;
     if (id("level-1").checked) solution = beginner[1];
     else if (id("level-2").checked) solution = intermediate[1];
-    else board = expert[1];
+    else solution = expert[1];
     // If tile number/letter is the same as solution's number/letter
     if (solution.charAt(tile.id) === tile.textContent) return true;
     else return false;
